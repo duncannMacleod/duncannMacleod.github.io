59b4512cd1d35f9264616b6f3c7aa6418f4224ad
duncannMacleod <duncan.boudet@estaca.eu>
2025-01-20 13:58:43 +0100

    cors changes

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_socketio import SocketIO, emit
+from flask_cors import CORS
+
 
 import collections
 if not hasattr(collections, 'MutableMapping'):
@@ -9,7 +11,8 @@ if not hasattr(collections, 'MutableMapping'):
 
 # Initialisation de l'application Flask et SocketIO
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'votre_cle_secrete'
+CORS(app)
+app.config['SECRET_KEY'] = '????????????'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 # Variable à gérer
